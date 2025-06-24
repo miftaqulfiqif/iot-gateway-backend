1c432809c8a0f3c6b84372de74f03205959f0c76
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2025-06-24 20:46:08 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config(); // Wajib ada sebelum file lain yang pakai .env
+console.log("✅ DATABASE_URL:", process.env.DATABASE_URL);
+
 import { io, server, port, mqttClient } from "./applications/app.js";
 import { setupSocket } from "./services/socket-services.js";
 import "./services/mqtt-services.js";
