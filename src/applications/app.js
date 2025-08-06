4d06c690a99849a7fdb515d1567a362e95b6474e
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2025-08-06 22:14:36 +0700

    add: detail patient

--- src/applications/app.js
+++ src/applications/app.js
@@ -12,8 +12,8 @@ import cookieParser from "cookie-parser";
 const port = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:5173",
-  // "http://192.168.1.36:5173",
-  "http://192.168.13.190:5173",
+  "http://192.168.1.25:5173",
+  // "http://192.168.13.190:5173",
 ];
 const mqttClient = mqtt.connect("mqtt://broker.emqx.io:1883");
 
