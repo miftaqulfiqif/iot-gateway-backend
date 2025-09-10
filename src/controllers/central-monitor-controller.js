f978708d9c885f6d2b65679cbea5f0090e57ed6f
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2025-09-10 14:26:33 +0700

    fix dashboard patient

--- src/controllers/central-monitor-controller.js
+++ src/controllers/central-monitor-controller.js
@@ -1,27 +1,30 @@
 import {
-    createPatientMonitoring, getCentralMonitorService
+  createPatientMonitoring,
+  getCentralMonitorService,
 } from "../services/api/central-monitor-service.js";
-import {prismaClient} from "../applications/database.js";
+import { prismaClient } from "../applications/database.js";
 
 const create = async (req, res, next) => {
-    try {
-        const result = await createPatientMonitoring(req.user.id, req.body);
-        res.status(200).json({ message: "Patient monitoring created", data: result });
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const result = await createPatientMonitoring(req.user.id, req.body);
+    res
+      .status(200)
+      .json({ message: "Patient monitoring created", data: result });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getCentralMonitors = async (req, res, next) => {
-    try {
-        const result = await getCentralMonitorService();
-        res.status(200).json({
-            message: "Getting central monitors",
-            data: result
-        });
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const result = await getCentralMonitorService();
+    res.status(200).json({
+      message: "Getting patient monitoring successfuly",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export default {create, getCentralMonitors};
+export default { create, getCentralMonitors };
